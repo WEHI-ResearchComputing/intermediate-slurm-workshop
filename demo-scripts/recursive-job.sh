4366793ccb5fee8dae60533d7b9d589041b1a495
Edward Yang <yang.e@wehi.edu.au>
2022-11-11 16:31:02 +1100

    Add job dependancy example

--- demo-scripts/recursive-job.sh
+++ demo-scripts/recursive-job.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+## recursive-job.sh
+
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=2G
-#SBATCH --time=1
-sleep 10
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=2-
+#SBATCH --output=output-%j.log
+#SBATCH --error=output-%j.log
+#SBATCH --mail-user=me.m@wehi.edu.au
+#SBATCH --mail-type=END,FAIL
+
+# Submitting a new job that depends on this one
+sbatch --dependency=afternotok:${SLURM_JOBID} recursive-job.sh
+
+# srunning the command
+srun flye [flags] --resume
